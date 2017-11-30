a14b859f8b21c72d15d050193a412fb127ab0518
qnewmk <30813629+qnewmk@users.noreply.github.com>
2017-11-30 16:16:23 -0200

    Add files via upload

---
+++ presente/main_he.py
@@ -0,0 +1,238 @@
+import os
+x = ''
+t = 0
+nome = input('digite seu nome (primeiro nome)\n')
+os.system('cls')
+nome.lower()
+if nome == 'carol' or nome == 'kaio':
+    if nome == 'carol' or nome == 'caronile':
+        print('loading.')
+        while t < 2599990:
+            t +=1
+        t = 0
+        os.system('cls')
+        print('loading .')
+        while t < 2599990:
+            t +=1
+        os.system('cls')
+        print('loading  .')
+        while t < 299999:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading.')
+        while t < 2599990:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading .')
+        while t < 2599990:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading  .')
+        while t < 2599990:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading.')
+        while t < 2599990:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading .')
+        while t < 2599990:
+            t += 1
+        t = 0
+        os.system('cls')
+        print('loading  .')
+        os.system('cls')
+        while t < 2599990:
+            t += 1
+        t = 0
+        print('=== feliz aniversário de namoro ===')
+        while t < 55999990:
+            t += 2
+        t = 0
+        os.system('cls')
+        print('=== te amo muito amor ===')
+        while t < 55999990:
+            t += 2
+        t = 0
+        os.system('cls')
+        print('=== obrigado pelo nosso segundo ano ===\n\n\n')
+        print('________**L**___________****L*** _______\n______LOVE*LOVE________LOVE***LOVE _____\n_____LOVE****LOVE_____LOVE*****LOVE ____\n_____LOVE******LOVE___LOVE*****LOVE ____\n_____LOVE*******LOVE_LOVE******LOVE ____\n______LOVE*********LOVE*******LOVE _____\n_______LOVE******************LOVE ______\n_________LOVE***************LOVE _______\n____________LOVE***********LOVE ________\n_______________LOVE******LOVE __________\n_________________LOVE**LOVE ____________\n__________________LOVELOVE _____________\n___________________**E** _______________\n___________________**** ________________\n___________________** __________________\n___________________* ___________________\n__________________*_____________________')
+        while t < 55999990:
+            t += 2
+        t = 0
+    while x == '':
+        os.system('cls')
+        print('=============')
+        print('== (A) - frases - (A) ==')
+        print('== (B) - musicas - (B) ==')
+        print('== (C) - sobre mim - (C) ==')
+        print('== (D) - imagens - (D) ==')
+        print('== (E) - carros - (E) ==')
+        print('=============')
+        res = input('Qual sua escolha ? \n')
+        res.lower()
+        if res == 'a':
+            x = 'back'
+            while x == 'back':
+                os.system('cls')
+                res = input('digite um número entre 1 e 5 \n')
+                res = int(res)
+                if res == 1:
+                    os.system('cls')
+                    print('você é a pessoa mais especial que existe pra mim\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                if res == 2:
+                    os.system('cls')
+                    print('você é a pessoa que mais ano no mundo, e você sabe disso\neu posso ver por esse sorisso no seu rosto agora\nhehe <3\n')
+                    x = input(
+                        'digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 3:
+                    os.system('cls')
+                    print('minha baixinha querida, sabe que é linda né?\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 4:
+                    os.system('cls')
+                    print('eu fou muito feliz por ter você ao meu lado\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 5:
+                    os.system('cls')
+                    print('um programa feito só pra menina que é dona do meu kokoro\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res < 1 or res > 5 :
+                    os.system('cls')
+                    print('desculpa amor, só fiz 5, mas eu tenho muito mais na minha cabeça\nse quiser perguntar uma agora eu vou responder\n')
+                    x = input('digite back para voltar ou enter para o menu principal\n')
+        elif res == 'b':
+            x = 'back'
+            while x == 'back':
+                os.system('cls')
+                print('aqui virão links do youtube, basta copiar a URL e,\nouvir as musicas que escolhi pra você\n')
+                print('=============')
+                print('== (A) - rock - (A) ==')
+                print('== (B) - eletrocinas - (B) ==')
+                print('== (C) - rap (N.A) - (C) ==')
+                print('== (D) - indie - (D) ==')
+                print('=============')
+                res = input('Qual sua escolha ? \n')
+                res.lower()
+                if res == 'a':
+                    os.system('cls')
+                    print('https://www.youtube.com/watch?v=B8XthE8UEFI')
+                    print('https://www.youtube.com/watch?v=2tSsRRSgj6U')
+                    print('https://www.youtube.com/watch?v=P-zb4C_k7Ek')
+                    print('https://www.youtube.com/watch?v=IJ9xhsVV42I\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 'b':
+                    os.system('cls')
+                    print('https://www.youtube.com/watch?v=SYDwZLfICVw')
+                    print('https://www.youtube.com/watch?v=qvBlHBHUTMs')
+                    print('https://www.youtube.com/watch?v=dmLi-1I4i4c')
+                    print('https://www.youtube.com/watch?v=4k4SP01l6rY\n')#se ler o código saiba que essa musica é importante pra mim...
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 'c':
+                    os.system('cls')
+                    print('https://www.youtube.com/watch?v=YfZwNF2Q84c')
+                    print('https://www.youtube.com/watch?v=f1tZ8YxzNCU')
+                    print('https://www.youtube.com/watch?v=2tvOTFtGdqs')
+                    print('https://www.youtube.com/watch?v=lWD9wVPwhH4\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 'd':
+                    os.system('cls')
+                    print('https://www.youtube.com/watch?v=SDTZ7iX4vTQ')
+                    print('https://www.youtube.com/watch?v=bpOSxM0rNPM')
+                    print('https://www.youtube.com/watch?v=IhP3J0j9JmY')
+                    print('https://www.youtube.com/watch?v=d2GCp8vs168\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+        elif res == 'c':
+            x = 'back'
+            while x == 'back':
+                os.system('cls')
+                res = input('digite um número entre 1 e 5\n')
+                res = int(res)
+                if res == 1:
+                    os.system('cls')
+                    print('o meu joelho esquerdo amanhece doendo alguns dias (sim é random)\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 2:
+                    os.system('cls')
+                    print('gosto bastante de carros... ava\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 3:
+                    os.system('cls')
+                    print('se eu fico sozinho por muito tempo eu fico irritado, e me fecho em mim mesmo\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 4:
+                    os.system('cls')
+                    print('amo muito a minha namorada\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 5:
+                    os.system('cls')
+                    print('eu faço piadas pra esconder as paradas daqui de dentro...\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+        elif res == 'd':
+            x = 'back'
+            while x == 'back':
+                os.system('cls')
+                print('vou colocar links e você copia e vê as imagens no browser\n')
+                res = input('digite um número entre 1 e 5\n')
+                res = int(res)
+                if res == 1:
+                    os.system('cls')
+                    print('https://vignette.wikia.nocookie.net/fastandfurious/images/5/57/1995_Mitsubishi_Eclipse.png/revision/latest/scale-to-width-down/640?cb=20160112004152\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 2:
+                    os.system('cls')
+                    print('http://vignette1.wikia.nocookie.net/fastandfurious/images/0/08/94_Supra_vs._70_Charger_-_Rear_View.jpg/revision/latest?cb=20160119020906\n')
+                    print('essa imagem tem muito de mim, se quiser pergunta pra mim e saberá o pq')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 3:
+                    os.system('cls')
+                    print('http://www.photosbytrinagueck.com/wp-content/uploads/94313-9dfb006915b4a0c97f98455832abf188.jpg\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 4:
+                    os.system('cls')
+                    print('https://i.pinimg.com/736x/7f/f3/86/7ff386a7125f6c5c2ea99066307cec24--role-playing-games-dungeons-and-dragons.jpg\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 5:
+                    os.system('cls')
+                    print('http://www.osmais.com/wallpapers/201201/59620d99b0-1400x1050.jpg\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+        elif res == 'e':
+            x = 'back'
+            while x == 'back':
+                os.system('cls')
+                print('os carros que não conhecer pesquise imagens')
+                res = input('digite um número entre 1 e 5\n')
+                res = int(res)
+                if res == 1:
+                    os.system('cls')
+                    print('Toyota - Supra')
+                    print(' =ava= *-*\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 2:
+                    os.system('cls')
+                    print('Nissan - GTR35')
+                    print('Nissan - R34\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 3:
+                    os.system('cls')
+                    print('BMW - 320i\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 4:
+                    os.system('cls')
+                    print('Mazda - RX8\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+                elif res == 5:
+                    os.system('cls')
+                    print('Dodge - Charger R/T 97\n')
+                    x = input('digite (back) para voltar\nenter para o menu principal\nqualquer outra coisa para fechar o programa\n')
+else:
+    os.system('cls')
+    print('desculpa, mas esse conteúdo é privado')
+    while t < 5990000:
+        t += 1
